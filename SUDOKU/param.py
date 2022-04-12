8e3b027a9b72ba8e8f5d7d5260e683d44124384b
Amirthavalli Kumar <101113632+Amirthavallikumar@users.noreply.github.com>
2022-04-13 02:56:36 +0530

    Add files via upload

---
+++ SUDOKU/param.py
@@ -0,0 +1,15 @@
+WIDTH = 600
+HEIGHT = 660
+
+# Colours
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+LIGHTBLUE = (96, 216, 232)
+LOCKEDCELLCOLOUR = (189,189,189)
+INCORRECTCELLCOLOUR = (195,121,121)
+GREEN = (147, 237, 161)
+
+# Positions and sizes
+gridPos = (75,160)
+cellSize = 50
+gridSize = cellSize*9
